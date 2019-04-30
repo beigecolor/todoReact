9fa60d3d1edd79feb614d254bcbaf5c85a7ae806
Jerusalem Lipardo <beigecolor29@gmail.com>
2019-04-30 15:34:06 -0700

    done my react todo

---
+++ src/Components/Home.js
@@ -0,0 +1,14 @@
+//home.js
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <h2>
+                I am HE MAN!!!!
+            </h2>
+        );
+    }
+}
+
+export default Home;
